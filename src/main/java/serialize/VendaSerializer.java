9b3ed4ce4b4f7f430ac22dd94da6eb11a366ed59
tarcio almeida <tarcioalmeidalima@gmail.com>
2021-11-23 16:57:17 -0300

    Produtor kafka gerador de vendas

---
+++ src/main/java/serialize/VendaSerializer.java
@@ -0,0 +1,21 @@
+package serialize;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import model.Venda;
+import org.apache.kafka.common.serialization.Serializer;
+
+public class VendaSerializer implements Serializer<Venda> {
+
+    @Override
+    public byte[] serialize(String s, Venda venda) {
+        try {
+            return new ObjectMapper().writeValueAsBytes(venda);
+
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
